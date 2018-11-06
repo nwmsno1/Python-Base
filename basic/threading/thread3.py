ecb531a5a9af735689f51849889420fdb5940ee7
nwmsno1 <410165862@qq.com>
2018-11-06 13:34:16 +0800

    Update thread3.py

--- basic/threading/thread3.py
+++ basic/threading/thread3.py
@@ -1,1 +1,32 @@
+import threading
+from queue import Queue
 
+
+def job(l, q):
+    for i in range(len(l)):
+        l[i] = l[i] ** 2
+    q.put(l)
+
+
+def multithreading():
+    q = Queue()
+    threads = []
+    data = [[1, 2, 3], [1, 3, 5], [2, 4, 6], [7, 8, 9]]
+    for i in range(4):
+        t = threading.Thread(target=job, args=(data[i], q))
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    results = []
+    for _ in range(4):
+        results.append(q.get())
+    print(results)
+
+
+def main():
+    multithreading()
+
+
+if __name__ == '__main__':
+    main()
