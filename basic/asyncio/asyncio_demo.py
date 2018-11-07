260e62f3f5c4d89497a831e6c3262fc9d34bfc1e
nwmsno1 <410165862@qq.com>
2018-11-07 22:49:39 +0800

    Update asyncio_demo.py

--- basic/asyncio/asyncio_demo.py
+++ basic/asyncio/asyncio_demo.py
@@ -4,6 +4,8 @@ import time
 
 async def job(t):  # 协程对象
     print('Start job ', t)
+    # 使用async可以定义协程对象，使用await可以针对耗时的操作进行挂起，就像生成器里的yield一样，函数让出控制权。
+    # 协程遇到await，事件循环将会挂起该协程，执行别的协程，直到其他的协程也挂起或者执行完毕，再进行下一个协程的执行
     await asyncio.sleep(t)  # wait for "t" seconds, it will look for another job while await
     print('Job ', t, ' takes ', t, ' s')
 
