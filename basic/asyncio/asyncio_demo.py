fe8e8b08e5769ea19d15247e9f0fe98a51ee2f6b
nwmsno1 <410165862@qq.com>
2018-11-07 17:08:18 +0800

    Update asyncio_demo.py

--- basic/asyncio/asyncio_demo.py
+++ basic/asyncio/asyncio_demo.py
@@ -2,13 +2,15 @@ import asyncio
 import time
 
 
-async def job(t):
+async def job(t):  # 协程对象
     print('Start job ', t)
     await asyncio.sleep(t)  # wait for "t" seconds, it will look for another job while await
     print('Job ', t, ' takes ', t, ' s')
 
 
 async def main(loop):
+    # 协程对象不能直接运行，在注册事件循环的时候，其实是run_until_complete方法将协程包装成为了一个任务（task）对象.
+    # task对象是Future类的子类，保存了协程运行后的状态，用于未来获取协程的结果
     tasks = [loop.create_task(job(t)) for t in range(1, 3)]  # just create, not run job
     await asyncio.wait(tasks)  # run jobs and wait for all tasks done
 
@@ -17,5 +19,4 @@ if __name__ == '__main__':
     t1 = time.time()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(loop))
-    
     print("Async total time : ", time.time() - t1)
