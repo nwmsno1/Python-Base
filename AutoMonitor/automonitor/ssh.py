658df960066877ff7b994ac8bd2557c13b3bde1e
nwmsno1 <410165862@qq.com>
2018-12-18 16:54:14 +0800

    Update ssh.py

--- AutoMonitor/automonitor/ssh.py
+++ AutoMonitor/automonitor/ssh.py
@@ -3,5 +3,22 @@ import paramiko
 
 class SSH(object):
 
-	def __init__(self, user, passwd, port=22):
-		
+	def __init__(self, user, passwd, ip, port=22):
+		self.user = user
+		self.passwd = passwd
+		self.ip = ip
+		self.port = port
+		self.last_msg = ''
+	
+	def is_alive(self, timeout=None):
+		try:
+			ssh = paramiko.SSHClient()
+			ssh.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
+			ssh.connect(hostname=self.ip,
+					   port=self.port,
+					   username=self.user,
+					   password=self.passwd,
+					   timeout=timeout,
+					   banner_timeout=timeout)
+			ssh.close()
+			return True
