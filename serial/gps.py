692df8f507c33f4ba6cf9dfb3652d7d01dde92c5
nwmsno1 <410165862@qq.com>
2019-02-15 16:14:49 +0800

    Update gps.py

--- serial/gps.py
+++ serial/gps.py
@@ -1,5 +1,32 @@
 import serial
+import time
 
-ser=serial.Serial("com1", 9600, timeout=0.5)
 
-ser.close()
+def recv(ser):
+    while True:
+        data = ser.read_all()
+        if data == '':
+            continue
+        else:
+            break
+        time.sleep(0.5)
+    return data
+
+
+def main():
+    ser=serial.Serial("com1", 9600, timeout=0.5)
+    if ser.isOpen():
+        print("open success")
+    else:
+        print("open failed")
+    ser.close()
+
+    while True:
+        data = recv(ser)
+        if data != b'':
+            print('receive:{0}'.format(data))
+            ser.write(data)
+
+
+if __name__ == '__main__':
+    main()        
