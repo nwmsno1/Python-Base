66e472e24a2fd6c4fee4b800c94f0fc54b2ec2bb
nwmsno1 <410165862@qq.com>
2019-05-29 16:10:29 +0800

    Create property2.py

---
+++ basic/decorator/property2.py
@@ -0,0 +1,44 @@
+"""
+class Student(object):
+
+    def get_score(self):
+         return self._score
+
+    def set_score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+        
+if __name__ == '__main__':
+    s = Student()
+    s.set_score(60)
+    s.set_score(90）
+"""
+
+"""
+@property的实现比较复杂，我们先考察如何使用。把一个getter方法变成属性，只需要加上@property就可以了，此时，@property本身又创建了另一个装
+饰器@score.setter，负责把一个setter方法变成属性赋值，于是，我们就拥有一个可控的属性操作
+"""
+
+
+class Student(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+
+
+if __name__ == '__main__':
+    s = Student()
+    s.score = 60
+    s.score = 1000
